6500bd73c31dccdd440a16c79af8a645e3d124b9
vikram-42-alla <allatrivikram@gmail.com>
2025-12-05 16:59:06 +0530

    added spring boot files

---
+++ ai-resume-server/src/main/java/com/example/ai_resume_server/AiResumeServerApplication.java
@@ -0,0 +1,19 @@
+package com.example.ai_resume_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class AiResumeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiResumeServerApplication.class, args);
+	}
+	@GetMapping
+	public String health(){
+		return "Server running successfully 🚀🚀🚀";
+	}
+
+}
